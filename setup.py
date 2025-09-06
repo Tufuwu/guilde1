4a90dd0a24199da4eace5b59091ebcada32b2617
wu <wu@.com>
2025-09-06 11:45:15 +0800

    run decalage2/olefile/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,52 +1,83 @@
-import os
+"""
+Installation script for olefile using setuptools
 
-from setuptools import find_packages, setup
+To install this package, run:
+    python setup.py install
 
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
+"""
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(
-    name="django-phone-verify",
-    version="2.0.1",
-    packages=find_packages(),
-    include_package_data=True,
-    license="GPLv3",
-    description="A Django app to support phone number verification using security code sent via SMS.",
-    long_description=README,
-    url="https://github.com/CuriousLearner/django-phone-verify",
-    author="Sanyam Khurana",
-    author_email="sanyam@sanyamkhurana.com",
-    install_requires=[
-        "django>=2.1.5",
-        "djangorestframework>=3.9.0",
-        "PyJWT>=1.7.1",
-        "python-dotenv>=0.10.0",
-        "phonenumbers>=8.10.2",
-        "django-phonenumber-field>=2.1.0",
-        "twilio>=6.21.0",
-        "nexmo>=2.4.0",
-    ],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
-)
+# --- IMPORTS -----------------------------------------------------------------
+
+from setuptools import setup
+
+from olefile import __version__, __author__
+
+
+# --- METADATA ----------------------------------------------------------------
+
+name         = "olefile"
+version      = __version__
+desc         = "Python package to parse, read and write Microsoft OLE2 files (Structured Storage or Compound Document, Microsoft Office)"
+author       = __author__
+author_email = "nospam@decalage.info"
+url          = "https://www.decalage.info/python/olefileio"
+license      = "BSD"
+download_url = "https://github.com/decalage2/olefile/tarball/master"
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+# --- PACKAGES ----------------------------------------------------------------
+
+packages = [
+    "olefile",
+]
+
+
+# === MAIN ====================================================================
+
+def main():
+    setup(
+        name=name,
+        version=version,
+        description=desc,
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        classifiers=classifiers,
+        author=author,
+        author_email=author_email,
+        url=url,
+        license=license,
+#        package_dir=package_dir,
+        packages=packages,
+        download_url=download_url,
+#        data_files=data_files,
+#        scripts=scripts,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    )
+
+
+if __name__ == "__main__":
+    main()
