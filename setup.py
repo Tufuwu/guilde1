e6e2b5c9c2915ff386411c46894c6ea77d25ac6b
wu <wu@.com>
2025-09-06 12:08:00 +0800

    run pytest-dev/unittest2pytest/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,105 +1,79 @@
-"""Setup script for Pympler.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015-2019 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+#
+# This file is part of unittest2pytest.
+#
+# unittest2pytest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
-To build, install and test Pympler and to try Pympler
-before building and installing it.
-
-The HTML documentation is in the doc/ directory.  Point
-your browser to the ./doc/html/index.html file.
-
-"""
-import sys
-
-
-def _not_supported(why):
-    print('NotImplementedError: ' + why + '.')
-    sys.exit(1)
-
-
-if sys.hexversion < 0x3050000:
-    _not_supported('Pympler requires Python 3.5 or newer')
-
-import os
-from setuptools import Command
 from setuptools import setup
-from setuptools import Distribution
-from subprocess import run
+import re
 
 
-class BaseTestCommand(Command):
-    """Base class for the pre and the post installation commands. """
-    user_options = []
-
-    def initialize_options(self):
-        self.param = None
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        args = [sys.executable,  # this Python binary
-                os.path.join('test', 'runtest.py'),
-                self.param, '-verbose', '3']
-        args.extend(sys.argv[2:])
-        sys.exit(run(args).returncode)
+def get_version(filename):
+    """
+    Return package version as listed in `__version__` in `filename`.
+    """
+    init_py = open(filename).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-class PreinstallTestCommand(BaseTestCommand):
-    description = "run pre-installation tests"
-
-    def initialize_options(self):
-        self.param = '-pre-install'
+version = get_version('unittest2pytest/__init__.py')
 
 
-class PostinstallTestCommand(BaseTestCommand):
-    description = "run post-installation tests"
-
-    def initialize_options(self):
-        self.param = '-post-install'
+def read(filename):
+    return open(filename, 'r', encoding='utf-8').read()
 
 
-def run_setup(include_tests=0):
-    tests = []
-    if include_tests:
-        tests = ['test', 'test.asizeof', 'test.tracker', 'test.muppy',
-                 'test.gui']
-
-    setup(packages=['pympler', 'pympler.util'] + tests,
-          package_data={'pympler': ['templates/*.html',
-                                    'templates/*.tpl',
-                                    'templates/*.js',
-                                    'templates/*.css',
-                                    'static/*.js']},
-          platforms=['any'],
-          classifiers=['Development Status :: 4 - Beta',
-                       'Environment :: Console',
-                       'Intended Audience :: Developers',
-                       'License :: OSI Approved :: Apache Software License',
-                       'Operating System :: OS Independent',
-                       'Programming Language :: Python',
-                       'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.5',
-                       'Programming Language :: Python :: 3.6',
-                       'Programming Language :: Python :: 3.7',
-                       'Programming Language :: Python :: 3.8',
-                       'Programming Language :: Python :: 3.9',
-                       'Programming Language :: Python :: 3.10',
-                       'Topic :: Software Development :: Bug Tracking',
-                       ],
-          cmdclass={'try': PreinstallTestCommand,
-                    'test': PostinstallTestCommand,
-                    }
-          )
+long_description = '\n\n'.join([read('README.rst'),
+                                read('CHANGES.rst')])
 
 
-try:  # hack Pympler commands into setup.py help output
-    Distribution.common_usage += """
-Pympler commands
-  setup.py try     try Pympler before installation
-  setup.py test    test Pympler after installation
-"""
-except AttributeError:
-    pass
-
-# Only include tests if creating a distribution package
-# (i.e. do not install the tests).
-run_setup('sdist' in sys.argv)
+setup(
+    name="unittest2pytest",
+    license='GPLv3+',
+    version=version,
+    description="Convert unittest test-cases to pytest",
+    long_description=long_description,
+    author="Hartmut Goebel",
+    author_email="h.goebel@crazy-compilers.com",
+    url="https://github.com/pytest-dev/unittest2pytest",
+    packages=["unittest2pytest", "unittest2pytest.fixes"],
+    entry_points={
+        'console_scripts': [
+            'unittest2pytest = unittest2pytest.__main__:main',
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False
+)
